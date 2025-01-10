855ed5b4c5e958a3fcdf60e54200b1bb07db7465
Ray Bunnage <bunnage.ray@gmail.com>
2025-01-09 18:50:25 -0800

    tests are now handled by github i think

--- backend/src/__init__.py
+++
@@ -1,7 +0,0 @@
-import sys
-from pathlib import Path
-
-# Add the project root to PYTHONPATH
-root = Path(__file__).parent.parent.parent
-if str(root) not in sys.path:
-    sys.path.append(str(root))
